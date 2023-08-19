08ed300a97c60b1c6f44a7fc856f9b706fd9a46b
Cyrus Blake <cyrusblake89@gmail.com>
2023-08-19 16:07:35 -0400

    Updates to Home page
    This allows to display a random word

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,15 +1,39 @@
 import DataDisplay from '../components/DataDisplay';
 import './Home.css';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
-    const wordToDisplay = "Word to be displayed";
+    const [randomWord, setRandomWord] = useState('');
+  
+    async function fetchWords() {
+        const response = await fetch('words.txt');
+        const text = await response.text();
+        return text.trim().split('\n');
+    }
+
+    function getRandomWord(words) {
+        const randomIndex = Math.floor(Math.random() * words.length);
+        return words[randomIndex];
+    }
+
+    useEffect(() => {
+        async function updateRandomWord() {
+        const words = await fetchWords();
+        const newRandomWord = getRandomWord(words);
+        setRandomWord(newRandomWord);
+        }
+
+        updateRandomWord();
+    }, []);
+
+
     return (
         <>
             <div className='title'>
                 <h1>Word of the Day</h1>
             </div>
             <div className='WordContainer'>
-                <DataDisplay content={wordToDisplay} />
+                <pre>{randomWord}</pre>
             </div>
         </>
     )
