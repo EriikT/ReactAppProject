5121e8886154cc660726a6af5fa05366aa656952
Erik <masteret@ymail.com>
2023-08-21 17:34:19 -0400

    Added Navbar component

--- my-app/src/pages/Login.js
+++ my-app/src/pages/Login.js
@@ -6,9 +6,14 @@ export default function Login() {
         <>
             <div className='login-container'>
                 <div className='login-logo'>LOGO</div>
-                <div className='button-container'>
+                <div className=''>
+                    <Link to="">
+                        <button className='get-started-button'>Get Started</button>
+                    </Link>
+                </div>
+                <div className=''>
                     <Link to="/Home">
-                        <button className='login-button'>Login</button>
+                        <button className='login-button'>Sign In</button>
                     </Link>
                 </div>
             </div>
