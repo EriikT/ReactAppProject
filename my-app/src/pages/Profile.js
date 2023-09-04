08feced9553bd8f10ee7687d3111869f6ff9af14
Cyrus Blake <cyrusblake89@gmail.com>
2023-09-04 14:36:04 -0400

    more changes

--- my-app/src/pages/Profile.js
+++ my-app/src/pages/Profile.js
@@ -10,44 +10,50 @@ export default function Profile() {
                 <Navbar/>
             </div>
            <div className="Profile-Container">
+                <div className="bet-container">
 
-                <div className="Profile__top">
-                    <h1>Profile</h1>
-                    <h3>Name</h3>
-                    <h3>View My Profile</h3>
-                    <hr></hr>
-                </div>
-                <div className="Profile_bottom">
-                    <h4>Personal Information
-                        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-                        <span class="material-symbols-outlined">
-                        chevron_right
-                        </span>
-                    </h4>
-                    <h4>Login and Security
-                        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-                        <span class="material-symbols-outlined">
-                        chevron_right
-                        </span>
-                    </h4>
-                    <h4>Notifications
-                        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-                        <span class="material-symbols-outlined">
-                        chevron_right
-                        </span>
-                    </h4>
-                    <h4>Personalization
-                        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-                        <span class="material-symbols-outlined">
-                        chevron_right
-                        </span>
-                    </h4>
-                    <h4>Log Out
-                        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-                        <span class="material-symbols-outlined">
-                        chevron_right
-                        </span>
-                    </h4>
+
+                    <div className="Profile__top">
+                        <h1>Profile</h1>
+                        <div className="edit-p">
+                            <h3>Name</h3>
+                            <h3>View My Profile</h3>
+                        </div>
+                        
+                        <hr></hr>
+                    </div>
+                    <div className="Profile_bottom">
+                        <h4>Personal Information
+                            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+                            <span class="material-symbols-outlined">
+                            chevron_right
+                            </span>
+                        </h4>
+                        <h4>Login and Security
+                            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+                            <span class="material-symbols-outlined">
+                            chevron_right
+                            </span>
+                        </h4>
+                        <h4>Notifications
+                            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+                            <span class="material-symbols-outlined">
+                            chevron_right
+                            </span>
+                        </h4>
+                        <h4>Personalization
+                            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+                            <span class="material-symbols-outlined">
+                            chevron_right
+                            </span>
+                        </h4>
+                        <h4>Log Out
+                            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+                            <span class="material-symbols-outlined">
+                            chevron_right
+                            </span>
+                        </h4>
+                    </div>
                 </div>
             </div>
         </>
